625e926055db0106266dd6f2ee5ea20297cc2ef6
Md Riajul Islam <118951084+riaj88@users.noreply.github.com>
2023-10-18 19:06:25 +0600

    Add files via upload

---
+++ Instagramscrapping.py
@@ -0,0 +1,32 @@
+import instaloader
+from instaloader import Profile, Post
+
+# Define the Instagram ID of the profile you want to scrape data from 
+profile_id = "riaj_proggavaller"
+
+# Create an instance of the Instaloader class
+loader = instaloader.Instaloader()
+
+# Load the profile using its ID
+profile = Profile.from_username(loader.context, profile_id)
+
+# Get the profile's username, full name, and number of followers
+username = profile.username
+full_name = profile.full_name
+followers = profile.followers
+
+# Print the profile's information
+print("Username:", username)
+print("Full name:", full_name)
+print("Followers:", followers)
+
+# Get the profile's posts
+posts = profile.get_posts()
+
+# Iterate over the posts and print their captions and URLs
+for post in posts:
+    caption = post.caption
+    url = post.url
+    print("Caption:", caption)
+    print("URL:", url)
+
